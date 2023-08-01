065d4db02b492abb39ddf2129fe44d4aa5c516b3
LONEWOLF143 <twaha2030@gmail.com>
2023-08-01 21:18:53 +0300

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+      <Container class="my-2">
+      <Outlet />
+      </Container>
+       
+    </>
+  )
+}
+
+export default App
