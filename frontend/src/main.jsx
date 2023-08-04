df6e429748e4030eaf4ded95d51315f4e450501b
LONEWOLF143 <twaha2030@gmail.com>
2023-08-04 21:51:19 +0300

    apiSlice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from "react-redux"
+import store from "./store"
 import {
   createBrowserRouter,
    createRoutesFromElements,
@@ -25,7 +27,9 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <React.StrictMode>
     <RouterProvider router={router}/>
   </React.StrictMode>,
+  </Provider>
 )
